79adc205555fe1599849a9202b0dc25ded01e969
Popov Bogdan <popovbodya@gmail.com>
2019-01-29 19:12:35 +0300

    reformat maybe example

--- reactive-programming/src/main/java/ru/popov/bodya/rxjava2/maybe/MaybeTry.java
+++ reactive-programming/src/main/java/ru/popov/bodya/rxjava2/maybe/MaybeTry.java
@@ -7,15 +7,20 @@ import ru.popov.bodya.reactive.extensions.utils.Logger;
 public class MaybeTry {
 
     public static void main(String[] args) {
+        tryNoSuccess();
+    }
+
+    private static void tryNoSuccess() {
         final Disposable disposable = Observable.just(1, 2, 3, 4, 5)
                 .map(x -> x + 1)
                 .elementAt(6)
+                .doOnComplete(() -> Logger.log("onComplete after elementAt"))
+                .doOnSuccess(integer -> Logger.log("onSuccess after elementAt"))
                 .map(x -> x + 1)
                 .subscribe(
                         integer -> Logger.log("onSuccess with: " + integer),
                         throwable -> Logger.log("onError"),
                         () -> Logger.log("onComplete"));
     }
-
 
 }
