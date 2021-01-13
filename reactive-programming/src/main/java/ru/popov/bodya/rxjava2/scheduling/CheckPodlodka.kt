4c55f9aa521e94d8af29775839070007f84c41ec
Popov Bogdan <popovbodya@gmail.com>
2021-01-13 17:13:22 +0400

    добавил небольшой пример с кодом из практики

--- reactive-programming/src/main/java/ru/popov/bodya/rxjava2/scheduling/CheckPodlodka.kt
+++ reactive-programming/src/main/java/ru/popov/bodya/rxjava2/scheduling/CheckPodlodka.kt
@@ -1,11 +1,49 @@
 package ru.popov.bodya.rxjava2.scheduling
 
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.SingleEmitter
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
+    val disposable = checkPaymentState()
+        .doOnNext {  }
+        .subscribe { value ->
+            log("value received: $value")
+        }
 
+    TimeUnit.SECONDS.sleep(2)
+    disposable.dispose()
+    TimeUnit.SECONDS.sleep(7)
+}
+
+private fun checkPaymentState(): Observable<Int> {
+    val paymentService = Service()
+    return Single.create<Int> { emitter ->
+        paymentService.startWork(TimeoutListener(emitter))
+    }
+        .toObservable()
+        .startWith(4)
+}
+
+private class Service {
+    fun startWork(timeoutListener: TimeoutListener) {
+        Thread {
+            TimeUnit.SECONDS.sleep(5)
+            timeoutListener.callback()
+        }.start()
+    }
+}
+
+private class TimeoutListener(private val emitter: SingleEmitter<Int>) {
+    fun callback() {
+        log("is emitter disposed: ${emitter.isDisposed}")
+        emitter.onSuccess(5)
+    }
+}
+
+private fun doOnSubscribeCase() {
     Observable.fromCallable {
         log("fromCallable")
         1
@@ -22,5 +60,5 @@ fun main(args: Array<String>) {
 }
 
 private fun log(msg: String) {
-    println("Thread: ${Thread.currentThread()}, msg: $msg")
+    println("Thread: ${Thread.currentThread().name}, msg: $msg")
 }
