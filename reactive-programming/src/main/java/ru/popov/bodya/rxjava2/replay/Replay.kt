26b27562a6f48a60664af2a95b2e2e253808c9d6
Popov Bogdan <popovbodya@gmail.com>
2021-03-05 15:23:04 +0300

    изучил replay - по факту он записывает в буфер n+1 элементом терминальное значение - onError или onComplete

--- reactive-programming/src/main/java/ru/popov/bodya/rxjava2/replay/Replay.kt
+++ reactive-programming/src/main/java/ru/popov/bodya/rxjava2/replay/Replay.kt
@@ -7,8 +7,8 @@ fun check() {
     val subject = PublishSubject.create<Int>()
 
     val obs: Observable<Int> = subject
-        .flatMap { data -> Observable.fromCallable { if (data == 5) throw RuntimeException("my message") else data } }
-        .replay(1)
+        .flatMap { data -> Observable.fromCallable { if (data == 5) throw RuntimeException("error msg") else data } }
+        .replay(2)
         .autoConnect()
 
     obs.subscribe({ println("onNext1: $it") }, { println("onError1: ${it.message}") })
@@ -16,10 +16,20 @@ fun check() {
     obs.subscribe({ println("onNext2: $it") }, { println("onError2: ${it.message}") })
     subject.onNext(2)
     subject.onNext(3)
-    subject.onNext(5)
+    subject.onComplete()
     obs.subscribe({ println("onNext3: $it") }, { println("onError3: ${it.message}") })
     subject.onNext(4)
-    obs.subscribe({ println("onNext4: $it") }, { println("onError4: ${it.message}") })
+    val disposable = obs
+        .map {
+            println("mapping value: $it")
+            it*it
+        }
+        .subscribe(
+            { println("onNext4: $it") },
+            { println("onError4: ${it.message}") },
+            { println("onComplete") },
+            { println("onSubscribe") }
+        )
 }
 
 fun main(args: Array<String>) {
